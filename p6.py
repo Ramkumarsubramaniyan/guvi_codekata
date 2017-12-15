eb25e2fa6c154ab3bb1a206205dc64a20b7145ca
PavanTejaa <31896426+PavanTejaa@users.noreply.github.com>
2017-12-16 00:41:06 +0530

    Update p6.py

--- p6.py
+++ p6.py
@@ -1,8 +1,8 @@
 year=int(input("Enter your birth year:"))
 if ((year%4)or (year%400 and year%100)):
-    print("It is a leap year")
+    print("leap year")
 else:
-    print("It is not a leap year")
+    print("not a leap year")
 
     
     
