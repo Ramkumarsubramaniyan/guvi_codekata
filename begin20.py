2c24b9224c3b754030c3f6a34a8c557b30c37ff3
PavanTejaa <31896426+PavanTejaa@users.noreply.github.com>
2018-06-11 14:15:53 +0530

    Update begin20.py

--- begin20.py
+++ begin20.py
@@ -1,6 +1,9 @@
 try:
 	n=int(input())
-	for x in range(1,6):
-		print(n*x)
+	if (n>=1):
+		for x in range(1,6):
+			print(n*x)
+	else:
+		print("invalid input")
 except ValueError:
 	print("invalid input")
