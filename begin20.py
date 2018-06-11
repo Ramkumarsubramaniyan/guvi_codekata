a26c2f3d83afe24ad3fc087e829fd3a6fac23cbd
PavanTejaa <31896426+PavanTejaa@users.noreply.github.com>
2018-06-11 14:34:53 +0530

    Update begin20.py

--- begin20.py
+++ begin20.py
@@ -1,11 +1,8 @@
 try:
 	n=int(input())
-	if (n>=1):
-		my_list=[]
-		for x in range(0,5):
-			my_list.append((x+1)*n)
-		print(" ".join(str(x) for x in my_list))
-	else:
-		print("invalid input")
+	my_list=[]
+	for x in range(0,5):
+		my_list.append((x+1)*n)
+	print(" ".join(str(x) for x in my_list))
 except ValueError:
 	print("invalid input")
