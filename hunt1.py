56976a2c5a8e4158e2c332733db9142640ae08cd
PavanTejaa <31896426+PavanTejaa@users.noreply.github.com>
2018-06-14 14:48:47 +0530

    Update hunt1.py

--- hunt1.py
+++ hunt1.py
@@ -1,28 +1,28 @@
 try:
 	n=int(input())
+	if(n<=0):
+		print("Invalid Input")	
 	l=list(map(int,input().split(' ')))
 	if(len(l)!=n):
 		print("Invalid Input")
 	else:
-		res=[]
+		res=[0]
 		for x in range(len(l)):
 			for y in range(x+1,len(l)):
 				if(l[x]==l[y]):
 					a=l[y]
 					count=0
-					for z in range(len(res)+1):
-						try:
-							if(res[z]==a):
-								count+=1
-							if(count==0):
-								res.append(a)
-						except IndexError:
-							res.append(a)
+					for z in range(len(res)):
+						if(res[z]==a):
+							count+=1
+					if(count==0):
+						res.append(a)
+		res.pop(0)
 		res.sort()
 		if not res:
 			print("unique")
 		else:
-			print(res)
+			print (" ".join(str(p) for p in res))
 except ValueError:
 	print("Invalid Input")
 
