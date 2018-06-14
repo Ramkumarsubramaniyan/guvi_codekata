3528f437d7ef47763f03698f4a078c7da055d8d0
PavanTejaa <31896426+PavanTejaa@users.noreply.github.com>
2018-06-14 15:14:16 +0530

    Create hunt3.py

---
+++ hunt3.py
@@ -0,0 +1,20 @@
+try:
+	n=int(input())
+	if(n<=0):
+		print("Invalid Input")	
+	l=list(map(int,input().split(' ')))
+	if(len(l)!=n):
+		print("Invalid Input")
+	else:
+		res=[0]
+		for x in range(len(l)):
+			if(l[x]==x):
+				res.append(l[x])
+		res.pop(0)
+		res.sort()
+		if not res:
+			print("'-1'")
+		else:
+			print(' '.join(str(p) for p in res))
+except ValueError:
+	print("Invalid Input")
